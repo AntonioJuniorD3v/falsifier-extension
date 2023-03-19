cdc549acbb9c291081fe669226c3ca0a0324954f
Antonio Júnior <antonio.junior@loft.com.br>
2023-03-19 10:18:40 -0300

    feat: created masks

---
+++ src/utils/masks.ts
@@ -0,0 +1,15 @@
+export const phoneMask = (v: string) => {
+  if (!v) return "";
+  v = v.replace(/\D/g, "");
+  v = v.replace(/(\d{2})(\d)/, "($1) $2");
+  v = v.replace(/(\d)(\d{4})$/, "$1-$2");
+  return v;
+};
+
+export const maskCpf = (v: string) => {
+  v = v.replace(/\D/g, "");
+  v = v.replace(/(\d{3})(\d)/, "$1.$2");
+  v = v.replace(/(\d{3})(\d)/, "$1.$2");
+  v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+  return v;
+};
